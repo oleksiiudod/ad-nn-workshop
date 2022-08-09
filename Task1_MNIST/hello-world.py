6cf4053ff4948c70f56e481380c0dd1c9d7266eb
Oleksii Udod <oleksii.udod@starship.co>
2022-08-09 18:18:28 +0300

    update MNIST examples

--- Task1_MNIST/hello-world.py
+++ Task1_MNIST/hello-world.py
@@ -2,10 +2,8 @@ import os
 
 import torch
 from pytorch_lightning import LightningModule, Trainer
-from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split
-from torchmetrics import Accuracy
+from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 
@@ -13,6 +11,7 @@ PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
 AVAIL_GPUS = min(1, torch.cuda.device_count())
 BATCH_SIZE = 256 if AVAIL_GPUS else 64
 print(AVAIL_GPUS, BATCH_SIZE)
+
 
 class MNISTModel(LightningModule):
     def __init__(self):
@@ -30,18 +29,18 @@ class MNISTModel(LightningModule):
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=0.02)
 
+
 # Init our model
 mnist_model = MNISTModel()
 
 # Init DataLoader from MNIST Dataset
 train_ds = MNIST(PATH_DATASETS, train=True, download=True, transform=transforms.ToTensor())
-train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE)
+train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE, num_workers=4)
 
 # Initialize a trainer
 trainer = Trainer(
     gpus=AVAIL_GPUS,
     max_epochs=10,
-    progress_bar_refresh_rate=20,
 )
 
 # Train the model ⚡
